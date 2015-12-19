e82df8575c26b9cc5cb462defb71d27528be1f3d
rkenning <Ryan.Kenning@rkitsolutions.co.uk>
2015-12-19 22:35:48 +0000

    Initial stab at getting tings displayed on the form

---
+++ SpaceShipAdventure/GameWindow.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace SpaceShipAdventure
+{
+    public partial class GameWindow : Form
+    {
+
+        private Ship TheShip = new Ship();
+
+        public GameWindow()
+        {
+            InitializeComponent();
+
+            SetStyle(ControlStyles.UserPaint, true);
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+
+//            InitializeAllGameObjects(true);
+
+      
+
+        time_GameTick.Start();
+
+        }
+
+
+        private void GameWindow_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            TheShip.Draw(g);
+
+            /* for (int i = 0; i < kNumberOfShields; i++)
+             {
+                 Shields[i].Draw(g);
+             }
+
+             //			g.FillRectangle(Brushes.Black, 0, 0, ClientRectangle.Width, ClientRectangle.Height);
+             TheMan.Draw(g);
+             TheScore.Draw(g);
+             TheHighScore.Draw(g);
+             _livesIndicator.Draw(g);
+
+             if (ActiveBullet)
+             {
+                 TheBullet.Draw(g);
+             }
+
+             if (SaucerStart)
+             {
+                 CurrentSaucer.Draw(g);
+             }
+
+             for (int i = 0; i < kNumberOfRows; i++)
+             {
+                 TheInvaders = InvaderRows[i];
+                 TheInvaders.Draw(g);
+             }*/
+        }
+    }
+}
---
+++ SpaceShipAdventure/Ship.cs
@@ -0,0 +1,39 @@
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+
+
+namespace SpaceShipAdventure
+
+{
+     class Ship : GameObject
+    {
+
+        public Ship(): base("ship.png")
+		{
+            Position.X = 10;
+            Position.Y = 220;
+
+        }
+
+        public override  void Draw(Graphics g)
+        {
+
+            base.Draw(g);
+            /*  if (Died)
+                return;
+
+            if (BeenHit == false)
+            {
+                base.Draw(g);
+            }
+            else
+            {
+                if (CountExplosion < 15)
+                    DrawExplosion(g);
+                else
+                    Died = true;
+            }*/
+
+        }
+    }
+}

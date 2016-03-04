634b8240d7917f472cbac1112e0a200ae251db47
rkenning <Ryan.Kenning@rkitsolutions.co.uk>
2016-03-04 07:25:22 +0000

    Added some missing files from Repo

---
+++ SpaceShipAdventure/GameEvent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceshipCommander
+{
+    class GameEvent
+    {
+        public enum Event_Types
+        {
+            Astoride,
+            HitAstorid,
+            EdgeOfSpace,
+            LaserHit,
+            Stopped,
+            Explode
+        };
+
+        public Event_Types event_type;
+        public int laser_source_vector;
+
+        public GameEvent(Event_Types event_type_)
+            {
+            event_type = event_type_;
+            }
+
+
+    }
+}
